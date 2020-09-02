ea9b0641a0e4aa56277cb102a59afed0de12ebf0
jelly_sim <jelly13.chen@itri.org.tw>
2020-09-02 13:39:02 +0800

    init

---
+++ app/src/main/java/com/example/smartwarehouse/model/Material.java
@@ -0,0 +1,64 @@
+package com.example.smartwarehouse.model;
+
+import java.util.Date;
+
+public class Material {
+    String date_code;
+    String model;
+    String item_name;
+    String factory;
+    String led_bin;
+    Date date_in;
+
+    public Material(String date_code) {
+        this.date_code = date_code;
+    }
+
+    public String getDate_code() {
+        return date_code;
+    }
+
+    public void setDate_code(String date_code) {
+        this.date_code = date_code;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public String getFactory() {
+        return factory;
+    }
+
+    public void setFactory(String factory) {
+        this.factory = factory;
+    }
+
+    public String getLed_bin() {
+        return led_bin;
+    }
+
+    public void setLed_bin(String led_bin) {
+        this.led_bin = led_bin;
+    }
+
+    public Date getDate_in() {
+        return date_in;
+    }
+
+    public void setDate_in(Date date_in) {
+        this.date_in = date_in;
+    }
+}
